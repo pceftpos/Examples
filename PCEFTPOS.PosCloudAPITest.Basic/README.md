851860cf8f40817cd982ed09bd719ea94d82d58a
Sujit Godavarti <sgodavarti@pceftpos.com.au>
2018-09-21 16:42:03 +1000

    Added basic jquery example project

---
+++ PCEFTPOS.PosCloudAPITest.Basic/README.md
@@ -0,0 +1,81 @@
+﻿
+# __Test POS Client-Server Application__
+
+This sample project represents a synchronous, client-server, web-based point of sale, using the Sandbox PC-EFTPOS Cloud REST API for communication with a physical or virtual Cloud-enabled pinpad. It uses jQuery ajax requests to make calls to the PC-EFTPOS Cloud REST API and waits for final transaction response.
+
+After a Transaction request is initiated from the client to the REST API, the client waits for a final transaction response. If it's a success, the client gets a *200 Success* response code. If the response code obtained is *202 Accepted* or if any other error occurs, the transaction enters an recovery mode to try and get the transaction status from the API, ie. did the EFT transaction succeed or fail.
+
+#### Technologies used:
+- Client: jQuery 2.2.0
+- REST API: ASP .Net Core 2.1
+
+#### Requirements
+- Visual Studio 2017 with .Net Core 2.1 framework
+
+#### Glossary
+|         Word            |                               Meaning                                 |
+| ------------------------|-----------------------------------------------------------------------|
+| POS                     | Point of sale                                                         |
+| Client, POS Client      | Point of sale client application, In this project it's a single html file 'index.html' under 'wwwroot' and associated jQuery file 'transaction.js' in the 'js' folder under 'wwwroot'  |
+| REST API, API           | PC-EFTPOS Cloud REST API  
+
+## __Getting started__
+#### Preparation
+##### 1. Set up your pinpad:
+* You will need a PC-EFTPOS Cloud Username and Password
+* Follow the bank/pinpad instructions to login and get the Pairing Code
+
+##### 2. Set up project settings
+Open "transaction.js" file found in **PCEFTPOS.WebAPI.PosCloudAPITest.Basic\wwwroot\js\transaction.js** and update the `pinpadUsername`, `pinpadPassword`, `pinpadPairCode` values in the `appSettings` object with the parameters from the pinpad.
+ ```
+ var appSettings = {
+		.
+		.
+		.
+		.
+        pinpadUsername: "<YOUR PINPAD USERNAME>",
+        pinpadPassword: "<YOUR PINPAD PASSWORD>",
+        pinpadPairCode: "<YOUR PINPAD PAIRING CODE>"
+    }
+```
+
+##### 3. Set up the project
+In the project properties under the Debug section make sure you use:
+* Profile: IIS Express
+* Launch: IIS Express
+* Enable SSL checkbox is selected
+
+To use localhost, please update the `cloudAPIUri` mentioned in the `appSettings` object with `https://localhost:<YOUR PORT NUMBER>/v1`.
+ ```
+ var appSettings = {
+		.
+		cloudAPIUri: "https://localhost:<YOUR PORT NUMBER>/v1",
+		.
+		.
+		.
+    }
+```
+
+Make sure you have the certificate to run the service over SSL. When prompted, you can safely accept the IIS Express generated certificate for this test application:
+
+![Certificate](Docs/certificate.png)
+
+#### Run
+Build the project.
+
+## __Usage__
+Once the application is running you will see the following page.<br/>
+![POS Start](Docs/pos_txn.png)
+
+##### Do transaction
+To create a transaction, just click the Transaction button to create a $1.00 transaction. You will see the following pop-up:<br/>
+![Txn Wait](Docs/pop-up_wait.png)
+
+Once the transaction is finished, the POS Client will show the following pop-up<br/>
+![POS Txn](Docs/pos_txn_done.png)
+
+## __Troubleshooting__
+* Make sure the pinpad is set up and running
+* Check you installed everything from the "Requirements" section
+* Check you updated the "transaction.js" file
+* Deploy the application
---
+++ PCEFTPOS.PosCloudAPITest.Basic/wwwroot/js/transaction.js
@@ -0,0 +1,163 @@
+﻿"use strict";
+
+$(document).ready(function () {
+
+    // Set appSettings and request objects
+    var appSettings = {
+        tokenServer: "https://pceftpos-authenticationservice-sandbox.azurewebsites.net/v1/",
+        cloudAPIUri: "https://localhost:<YOUR PORT NUMBER>/v1",
+        posName: "Test POS",
+        posVersion: "1.0",
+        merchant: "00",
+        application: "00",
+        pinpadUsername: "<YOUR PINPAD USERNAME>",
+        pinpadPassword: "<YOUR PINPAD PASSWORD>",
+        pinpadPairCode: "<YOUR PINPAD PAIRING CODE>"
+    }
+    var tokenRequest = {
+        username: appSettings.pinpadUsername,
+        password: appSettings.pinpadPassword,
+        pairCode: appSettings.pinpadPairCode
+    };
+    var eFTTransactionRequest = {
+        request: {
+            txnType: "P",
+            txnRef: (generateAlphaNumStr(16)).toUpperCase(),
+            amtPurchase: parseInt($("#inputAmount").val() * 100),
+            merchant: appSettings.merchant,
+            application: appSettings.application,
+            posName: appSettings.posName,
+            posVersion: appSettings.posVersion
+        }
+    };
+    var token = ""; // The authentication token
+    var sessionUUID = ""; // The generated sessionUUID value is a global variable to make 'GET' transaction request with the same UUID if a post doesn't respond with a '200' status
+
+    // Function to generate UUID which is the 'sessionUUID' used in the requests
+    function generateUUID() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+    // Regex validation for an input field so that it has a number with exactly two decimal points
+    var regexDecimal = /^(0|0?[1-9]\d*)\.\d\d$/;
+    $("#inputAmount").keyup(function () {
+        if (!regexDecimal.test($("#inputAmount").val())) {
+            $("#errorAmt").html("Please enter a value with exactly two decimal points.");
+            $("#btnSubmit").attr("disabled", true);
+        }
+        else {
+            $("#errorAmt").html("");
+            $("#btnSubmit").attr("disabled", false);
+        }
+    });
+
+    // Function to generate 'txnRef', which is sent as a part of 'eFTTransactionRequest' object
+    function generateAlphaNumStr(len) {
+        var rdmString = "";
+        for (; rdmString.length < len; rdmString += Math.random().toString(36).substr(2));
+        return rdmString.substr(0, len);
+    }
+
+    $('#btnSubmit').click(function () {
+        $("#responseModal").modal("show");
+        $("#btnClose").hide();
+        $("#txtContent").html("PLEASE WAIT...");
+
+        // Get authentication token
+        $.ajax({
+            url: appSettings.tokenServer + "tokens/cloudpos",
+            type: 'post',
+            data: JSON.stringify(tokenRequest),
+            contentType: "application/json; charset=utf-8",
+            success: function (response, textStatus, xhr) {
+                token = response.token;
+                sessionUUID = generateUUID();
+                getTransactionResponse(token, sessionUUID);
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.error(errorThrown);
+            }
+        });
+    });
+
+    // Function to 'POST' a transaction request and wait for the response
+    function getTransactionResponse(token, sessionUUID) {
+        $.ajax({
+            url: appSettings.cloudAPIUri + "sessions/" + sessionUUID + "/transaction",
+            type: 'post',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+            },
+            data: JSON.stringify(eFTTransactionRequest),
+            contentType: "application/json; charset=utf-8",
+            success: function (response, textStatus, xhr) {
+                if (xhr.status == 200 && response.response.success) {
+                    $("#txtContent").html(response.response.responseText);
+                    $("#btnClose").show();
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                if (xhr.status == 400) {
+                    $("#txtContent").html("INVALID REQUEST. THE TRANSACTION HAS NOT COMPLETED SUCCESSFULLY");
+                    $("#btnClose").show();
+                }
+                else if (xhr.status == 404) {
+                    $("#txtContent").html("REQUESTED RESOURCE IS UNAVAILABLE. THE TRANSACTION WAS UNSUCCESSFUL");
+                    $("#btnClose").show();
+                }
+                else if (xhr.status == 408 || (xhr.status >= 500 && xhr.status <= 599)) {
+                    enterErrorRecovery(sessionUUID);
+                }
+                else {
+                    $("#txtContent").html("AN ERROR OCCURED. PLEASE TRY AGAIN LATER");
+                    $("#btnClose").show();
+                }
+            }
+        });
+    }
+
+    // If the above request doesn't return a response with status '200', we enter into an error recovery mode and make a 'GET' request with same sessionUUID
+    function enterErrorRecovery(sessionUUID) {
+        $.ajax({
+            url: appSettings.cloudAPIUri + "sessions/" + sessionUUID + "/transaction",
+            type: 'get',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+            },
+            contentType: "application/json; charset=utf-8",
+            success: function (response, textStatus, xhr) {
+                if (xhr.status == 200 && response.response.success) {
+                    $("#txtContent").html(response.response.responseText);
+                    $("#btnClose").show();
+                }
+                else if (xhr.status == 202) {
+                    setTimeout(function () { enterErrorRecovery(sessionUUID) }, 10000);
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                if (xhr.status == 400) {
+                    $("#txtContent").html("INVALID REQUEST. THE TRANSACTION HAS NOT COMPLETED SUCCESSFULLY");
+                    $("#btnClose").show();
+                }
+                else if (xhr.status == 404) {
+                    $("#txtContent").html("REQUESTED RESOURCE IS UNAVAILABLE. THE TRANSACTION WAS UNSUCCESSFUL");
+                    $("#btnClose").show();
+                }
+                else if (xhr.status == 408 || (xhr.status >= 500 && xhr.status <= 599)) {
+                    setTimeout(function () { enterErrorRecovery(sessionUUID) }, 10000);
+                }
+                else {
+                    $("#txtContent").html("AN ERROR OCCURED. PLEASE TRY AGAIN LATER");
+                    $("#btnClose").show();
+                }
+            }
+        });
+    }
+
+    $('#btnClose').click(function () {
+        $("#responseModal").modal("hide");
+    });
+});
